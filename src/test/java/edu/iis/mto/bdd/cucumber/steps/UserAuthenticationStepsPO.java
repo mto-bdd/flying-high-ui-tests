c824c9f8253f67abf7226900d2907aa6df93b613
Radek Adamus <adamus.r@gmail.com>
2016-06-06 14:30:31 +0200

    adding welcome screen verification

--- src/test/java/edu/iis/mto/bdd/cucumber/steps/UserAuthenticationStepsPO.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/steps/UserAuthenticationStepsPO.java
@@ -1,42 +1,53 @@
 package edu.iis.mto.bdd.cucumber.steps;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import edu.iis.mto.bdd.cucumber.pages.HomePageWebDriver;
 import edu.iis.mto.bdd.cucumber.pages.LoginPage;
 import edu.iis.mto.bdd.cucumber.pages.LoginPageWebDriver;
 import edu.iis.mto.bdd.model.FrequentFlyerMember;
 
 public class UserAuthenticationStepsPO {
 	private final WebDriver driver = new FirefoxDriver();
-	
-    @Given("^(.*) is a registered Frequent Flyer$")
-    public void givenARegisteredFrequentFlyer(FrequentFlyerMember user) {}
+	private LoginPageWebDriver loginPage;
 
-    @When("^(.*) authenticates with a valid email address and password$")
-    public void whenJaneAuthenticatesWithAValidEmailAddressAndPassword(FrequentFlyerMember user) {
-    	LoginPageWebDriver loginPage = new LoginPageWebDriver(driver);
-    	loginPage.open();
-    	loginPage.signinWithCredentials(user.getEmail(), user.getPassword());
-		
-    }
+	@Given("^(.*) is a registered Frequent Flyer$")
+	public void givenARegisteredFrequentFlyer(FrequentFlyerMember user) {
+	}
 
-    @Then("^(.*) should be given access to (?:her|his) account$")
-    public void thenTheUserShouldBeGivenAccessToAccount(FrequentFlyerMember user) {
-    	
-    }
+	@When("^(.*) authenticates with a valid email address and password$")
+	public void whenJaneAuthenticatesWithAValidEmailAddressAndPassword(FrequentFlyerMember user) {
+		loginPage = new LoginPageWebDriver(driver);
+		loginPage.open();
+		loginPage.signinWithCredentials(user.getEmail(), user.getPassword());
 
-    @Given("^(.*) has logged on$")
-    public void aUserHasLoggedOnAs(FrequentFlyerMember user) {
-    	LoginPageWebDriver loginPage = new LoginPageWebDriver(driver);
-    	loginPage.open();
-    	loginPage.signinWithCredentials(user.getEmail(), user.getPassword());
-    }
+	}
 
-    @When("^(?:.*) views the home page$")
-    public void whenAUserViewsTheHomePage() {}
+	@Then("^(.*) should be given access to (?:her|his) account$")
+	public void thenTheUserShouldBeGivenAccessToAccount(FrequentFlyerMember user) {
+		HomePageWebDriver homepage = new HomePageWebDriver(driver);
+
+		assertThat(homepage.getWelcomeMessage(), equalTo("Witaj " + user.getFirstName()));
+
+	}
+
+	@Given("^(.*) has logged on$")
+	public void aUserHasLoggedOnAs(FrequentFlyerMember user) {
+		loginPage = new LoginPageWebDriver(driver);
+		loginPage.open();
+		loginPage.signinWithCredentials(user.getEmail(), user.getPassword());
+	}
+
+	@When("^(?:.*) views the home page$")
+	public void whenAUserViewsTheHomePage() {
+	}
 
 }
